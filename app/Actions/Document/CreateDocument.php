a7d462502f54e4fbfb68197883c1e1f5fbffbf1f
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2022-01-25 00:59:00 -0300

    create document

--- app/Actions/Document/CreateDocument.php
+++ app/Actions/Document/CreateDocument.php
@@ -2,14 +2,22 @@
 
 namespace App\Actions\Document;
 
+use App\Models\Document;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class CreateDocument
 {
     use AsAction;
 
-    public function handle()
+
+    public function __construct(private Document $document)
     {
-        // ...
+    }
+
+    public function handle(array $document): Document
+    {
+        $createdDocument = $this->document->fill($document);
+        $createdDocument->saveOrFail();
+        return $createdDocument->refresh();
     }
 }
